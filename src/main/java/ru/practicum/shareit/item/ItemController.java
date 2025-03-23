0a5ab85361326104ff14150848accaf33b5ceac8
ValentinaVK <ya.miss-vvk@yandex.ru>
2025-03-23 20:24:44 +0700

    Adjustments have been made: the "Mapstruct" library has been connected, the validation of the "email" field has been changed, etc.

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.item;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
